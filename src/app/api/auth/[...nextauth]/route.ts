810dc42d4ad7987c9584c9f705f382f0ca334814
Tina Lee <92625071+Learning-N-Running@users.noreply.github.com>
2024-09-22 04:31:27 +0800

    Feat: world id config

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,31 @@
+import NextAuth from "next-auth";
+import { NextAuthOptions } from "next-auth";
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    {
+      id: "worldcoin",
+      name: "Worldcoin",
+      type: "oauth",
+      wellKnown: "https://id.worldcoin.org/.well-known/openid-configuration",
+      authorization: { params: { scope: "openid" } },
+      clientId: process.env.WLD_CLIENT_ID,
+      clientSecret: process.env.WLD_CLIENT_SECRET,
+      idToken: true,
+      profile(profile) {
+        return {
+          id: profile.sub,
+          name: profile.sub,
+          verificationLevel:
+            profile["https://id.worldcoin.org/v1"].verification_level,
+        };
+      },
+    },
+  ],
+  debug: true,
+};
+
+// Define the API route handler for the App Router
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
